ae8569ddc8ec5972048d9a2f2f465d9840552706
NGamaniprasad <134373459+NGamaniprasad@users.noreply.github.com>
2026-01-31 23:59:47 +0530

    Update settings.py

--- My_project/settings.py
+++ My_project/settings.py
@@ -146,7 +146,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'grownbyaigp@gmail.com'
-EMAIL_HOST_PASSWORD = 'agblkeblhtorwrnv'
+EMAIL_HOST_PASSWORD = ''
 
 # DEFAULT_FROM_EMAIL = 'Nurturing Way <yourgmail@gmail.com>'
 
